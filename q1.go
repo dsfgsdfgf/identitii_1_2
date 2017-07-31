da32701db344cf00752540f002de59ad79c17944
dsfgsdfgf <kaanixir@gmail.com>
2017-08-01 08:59:30 +1000

    more comments

--- q1.go
+++ q1.go
@@ -22,15 +22,18 @@ func parse(v string) (*node, error) {
 	root.Name = ""
 	root.Children = make([]*node, 0)
 
-	var name string 	
-	var currentNode = &root	
+	var name string 		// node name
+	var currentNode = &root	// current node, start with root
 
 	for _, v := range v[1:] {	// skip first "["
 		currentChar := string(v)
 		var newNode *node
-	
+
+		// Iterate char.
+		// Assume default case is ASCII tokens, adding to the name. Otherwise   "["  "]"  or  ","
 		switch currentChar {
-		case "[":		
+		case "[":				// Go down, depth + 1, new children
+			// No children without names ( as with arrays of [[ or [[[ )
 			if len(name) > 0 {
 				newNode = &node{}
 				newNode.Name = name
@@ -40,11 +43,10 @@ func parse(v string) (*node, error) {
 				currentNode.Children = append(currentNode.Children, newNode)
 
 				currentNode = newNode	//
-
 				name = "" // -- name reset
 			}
 
-		case "]":	
+		case "]":				// Go up, depth - 1,
 			if len(name) > 0 {
 				newNode = &node{}
 				newNode.Name = name
@@ -55,22 +57,20 @@ func parse(v string) (*node, error) {
 
 				// end nesting, go up depth
 				currentNode = currentNode.Parent
-
 				name = "" // -- name reset
 			}
 
-		case ",":		
+		case ",":				// Sibling children node creation
 			if len(name) > 0 {
 				newNode = &node{}
 				newNode.Name = name
 				newNode.Parent = currentNode
-
 				currentNode.Children = append(currentNode.Children, newNode)
 
 				name = "" // -- name reset
 			}
 		default:
-			name += currentChar // add current character of name to the current name
+			name += currentChar // add current character to the current name
 
 		}
 	}
@@ -91,4 +91,3 @@ func main() {
 		log.Printf("Example %d: %s - %s", i, example, string(j))
 	}
 }
-
